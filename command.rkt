b304346c136a99c8f38e6f0b56b10af45e35a377
Jay McCarthy <jay.mccarthy@gmail.com>
2014-05-31 09:08:04 -0600

    Adding PNGs

--- command.rkt
+++ command.rkt
@@ -62,15 +62,12 @@
 
     (define-values (aw ah)
       (send c get-client-size))
-    (define base
-      (colorize (filled-rectangle slide-w slide-h) "white"))
-    (define-values (actual-slide almost-pict)
-      (ST-cmds-interp the-ST current-slide base))
-    (define nearly-pict
-      (parameterize ([current-slide-number actual-slide])
-        (ST-pipeline-apply the-ST almost-pict)))
+
+    (define-values (actual-slide nearly-pict)
+      (ST->slide-pict the-ST current-slide))
+
     (define final-pict
-      (scale-to-fit (clip nearly-pict) aw ah))
+      (scale-to-fit nearly-pict aw ah))
     (draw-pict-centered final-pict the-dc aw ah))
 
   (define pf (new pres-frame% [label "Puresuri"]))
@@ -100,12 +97,59 @@
     (load-mp!)
     (loop)))
 
+(define (ST->slide-pict st i)
+  (define base
+    (colorize (filled-rectangle slide-w slide-h) "white"))
+  (define-values (actual-slide almost-pict)
+    (ST-cmds-interp st i base))
+  (define nearly-pict
+    (clip
+     (parameterize ([current-slide-number actual-slide])
+       (ST-pipeline-apply st almost-pict))))
+  (values actual-slide nearly-pict))
+
+(define (ST->picts st)
+  (let loop ([i 0])
+    (define-values (ni p) (ST->slide-pict st i))
+    (cons p (if (= i ni) empty (loop ni)))))
+
+(define (puresuri->png-dir png-dir mp)
+  (local-require racket/file
+                 racket/format)
+  (printf "Creating directory ~a\n" png-dir)
+  (make-directory* png-dir)
+  (define the-ST (make-fresh-ST))
+  (printf "Loading slides...\n")
+  (parameterize ([current-ST the-ST])
+    (dynamic-require `(file ,mp) 0))
+  (printf "Rendering slides...\n")
+  (define ps (ST->picts the-ST))
+  (define how-many (length ps))
+  (define len (string-length (number->string how-many)))
+  (for ([p (in-list ps)]
+        [i (in-naturals)])
+    (define ni
+      (~a i #:min-width len #:align 'right #:pad-string "0"))
+    (printf "Saving slide ~a...\n" ni)
+    (define bm (pict->bitmap p))
+    (send bm save-file
+          (build-path png-dir (format "~a.png" ni))
+          'png
+          100)))
+
 (module+ main
   (require racket/cmdline)
 
   ;; xxx printing
 
+  (define operation puresuri!)
+
   (command-line
    #:program "puresuri"
+   #:once-any
+   [("--pngs") png-dir "Render as directory of pngs"
+    (set! operation
+          (λ (mp)
+            (puresuri->png-dir png-dir mp)))]
    #:args (module-path)
-   (puresuri! module-path)))
+   (operation module-path)))
